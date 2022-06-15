8a435d09cde489ad3820c703e99dd787878cbe4d
Vadick <vadick1@gmail.com>
2022-06-15 00:16:39 -0500

    creando el home component y agregando los estilos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import '../node_modules/@mdi/font/css/materialdesignicons.min.css'
+import '../node_modules/materialize-css/dist/css/materialize.min.css'
+import '../node_modules/materialize-css/dist/js/materialize.min.js'
+
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,7 +15,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
